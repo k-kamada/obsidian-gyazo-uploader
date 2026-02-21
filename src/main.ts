afedc5bd6a24cf09f86797c7de63f738e3ec3845
k-kamada <kamadak0404@gmail.com>
2026-02-22 00:40:36 +0900

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,164 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Editor, getAllTags, Notice, Plugin, requestUrl } from "obsidian";
+import { DEFAULT_SETTINGS, MySettings, SampleSettingTab } from "./settings";
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GyazoObsidianUploader extends Plugin {
+	settings: MySettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		this.registerEvent(
+			this.app.workspace.on(
+				"editor-drop",
+				async (event: DragEvent, editor: Editor) => {
+					event.preventDefault();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+					const files = event.dataTransfer?.files;
+					if (!files || files.length === 0) return;
+					if (files[0] && files[0].type.startsWith("image/")) {
+						// ドラッグ＆ドロップした位置にプレイスホルダーを挿入
+						const dropPos = editor.getCursor();
+						const placeholderText = `[!INFO] Uploading image to Gyazo...(timestamp: ${Date.now()})`;
+						editor.replaceRange(placeholderText, dropPos);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+						const formData = new FormData();
+						formData.append(
+							"access_token",
+							this.settings.accessToken,
+						);
+						formData.append("imagedata", files[0]);
+						if (this.settings.collectionId) {
+							formData.append(
+								"collection_id",
+								this.settings.collectionId,
+							);
+						}
+						formData.append("app", "Obsidian");
+
+						const activeFile = this.app.workspace.getActiveFile();
+
+						formData.append("title", activeFile?.basename || "");
+						const cache = activeFile
+							? this.app.metadataCache.getFileCache(activeFile)
+							: null;
+						const tags: string[] = cache
+							? getAllTags(cache) || []
+							: [];
+						formData.append(
+							"desc",
+							`filename: ${files[0].name}\ntags: ${tags.join(" ")}`,
+						);
+
+						const tempResponse = new Response(formData);
+
+						const body = await tempResponse.arrayBuffer();
+						const contentType =
+							tempResponse.headers.get("content-type") || "";
+
+						const requestParam = {
+							url: "https://upload.gyazo.com/api/upload",
+							method: "POST",
+							headers: {
+								Authorization: `Bearer ${this.settings.accessToken}`,
+								"Content-Type": contentType,
+							},
+							body: body,
+						};
+						try {
+							const uploadResponse =
+								await requestUrl(requestParam);
+							const imageId: string =
+								uploadResponse.json.image_id;
+
+							new Notice(
+								"[Gyazo-Uploader] Upload finished. Waiting for OCR...",
+							);
+							// OCRの処理を待つため10秒ウェイト
+							await new Promise((resolve) =>
+								setTimeout(resolve, 10000),
+							);
+
+							const infoResponse = await requestUrl({
+								url: `https://api.gyazo.com/api/images/${imageId}`,
+								method: "GET",
+								headers: {
+									Authorization: `Bearer ${this.settings.accessToken}`,
+								},
+							});
+
+							const ocrText = (
+								infoResponse.json.metadata.ocr
+									.description as string
+							).replace(/\n/g, "");
+
+							const url = infoResponse.json.url;
+
+							const resultText = `![ocr:"${ocrText}"${this.settings.imageDisplayWidth ? `|${this.settings.imageDisplayWidth}` : ""}](${url})`;
+
+							const result = replacePlaceholder(
+								editor,
+								placeholderText,
+								resultText,
+							);
+
+							new Notice("[Gyazo-Uploader] Generate image link");
+						} catch (error) {
+							console.error("Error:", error);
+							replacePlaceholder(
+								editor,
+								placeholderText,
+								"[INFO!] Failed to upload",
+							);
+							new Notice(
+								"[Gyazo-Uploader] Error: failed to upload an image to Gyazo",
+							);
+						}
 					}
+				},
+			),
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
+		const replacePlaceholder = (
+			editor: Editor,
+			placeholderText: string,
+			replacement: string,
+		) => {
+			const content = editor.getValue();
+			const lines = content.split("\n");
+
+			for (let i = 0; i < lines.length; i++) {
+				if (lines[i]?.includes(placeholderText)) {
+					const startChar = lines[i]?.indexOf(placeholderText) || 0;
+					const from = { line: i, ch: startChar };
+					const to = {
+						line: i,
+						ch: startChar + placeholderText.length,
+					};
+
+					editor.replaceRange(replacement, from, to);
 					return true;
 				}
-				return false;
 			}
-		});
+			return false;
+		};
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
-	onunload() {
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			(await this.loadData()) as Partial<MySettings>,
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,68 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+import GyazoObsidianUploader from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface MySettings {
+	accessToken: string;
+	collectionId: string;
+	imageDisplayWidth: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: MySettings = {
+	accessToken: "",
+	collectionId: "",
+	imageDisplayWidth: "",
+};
 
 export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: GyazoObsidianUploader;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GyazoObsidianUploader) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Access token")
+			.setDesc("It's a secret")
+			.addText(
+				(text) =>
+					(text
+						.setPlaceholder("Enter your secret")
+						.setValue(this.plugin.settings.accessToken)
+						.onChange(async (value) => {
+							this.plugin.settings.accessToken = value;
+							await this.plugin.saveSettings();
+						}).inputEl.type = "password"),
+			);
+		new Setting(containerEl)
+			.setName("Collection ID")
+			.setDesc("Collection ID")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.collectionId)
+					.onChange(async (value) => {
+						this.plugin.settings.collectionId = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+
+		new Setting(containerEl)
+			.setName("Image display width")
+			.setDesc("Number")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.imageDisplayWidth)
+					.onChange(async (value) => {
+						this.plugin.settings.imageDisplayWidth = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
